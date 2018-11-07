0148cf693da26ccafa3ccb58862629f60847f701
Gus <gustavojuan@gmail.com>
2018-11-07 11:13:23 +0100

    Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -20,19 +20,55 @@
                 {!! wp_nav_menu(['theme_location' => 'primary_right_nav', 'menu_class' => 'nav']) !!}
             @endif
         </div>
-        <form class="navbar-form navbar-right navbar-form-search position-absolute" role="search" style="right: 1em;">
+
+
+
+
+
+
+    </nav>
+
+    <div id="utilities_wrapper" >
+        <form class="navbar-form navbar-right navbar-form-search float-left mr-3" role="search">
             <div class="search-form-container hdn" id="search-input-container">
                 <div class="search-input-group">
-                    <button type="button" class="btn btn-default" id="hide-search-input-container"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></button>
+                    <a class="close-button" id="hide-search-input-container"><i class="fa fa-times" aria-hidden="true"></i></a>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Search for...">
                     </div>
                 </div>
             </div>
-            <button type="submit" class="btn btn-default d-none d-md-block" id="search-button"><span class="glyphicon glyphicon-search" aria-hidden="true">M</span></button>
+            <a  class="d-none d-md-block" id="search-button"><i class="fa fa-search" aria-hidden="true"></i></a>
         </form>
+        <div class="dropdown show navbar-right float-right">
 
 
-    </nav>
+            <?php
+	            $languages = icl_get_languages('skip_missing=0&orderby=code');
+	            $custom_languages = [];
+	            foreach ($languages as  $l){
+	            	if(!$l['active']){
+			            array_push($custom_languages,$l);
+                    }else{
+			            $custom_languages[0] = $l;
+                    }
+                }
+            ?>
+            <a class="dropdown-toggle text-uppercase" href="<?=$custom_languages[0]['url']?>" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?= substr($custom_languages[0]['native_name'],0,2)?>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+              <?php
+                foreach ($custom_languages as $key => $language){
+	                if ($key > 0){
+
+                        echo '<a class="dropdown-item" href="'.$language['url'].'">'.$language['native_name'].'<img class="ml-3 rounded-circle" src="'.$language['country_flag_url'].'" width="30" height="30"></a>';
+                    }
+                }
+              ?>
+            </div>
+        </div>
+    </div>
 </header>
 
