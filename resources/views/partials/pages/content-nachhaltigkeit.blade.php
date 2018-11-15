593e949a71868c793b35d2d668e842a9ffdceec6
Gus <gustavojuan@gmail.com>
2018-11-15 15:41:53 +0100

    Junto

---
+++ resources/views/partials/pages/content-nachhaltigkeit.blade.php
@@ -0,0 +1,228 @@
+<h2 class="text-center big">
+    <?=_e("Geschmack im Einklang mit Mensch und Natur", "sanlucar")?>
+</h2>
+
+<p><?=_e("Wir möchten alle Menschen mit dem natürlichsten, frischesten und geschmackvollsten Obst und Gemüse versorgen – und zwar so, dass wir stolz darauf sein können. Das ist leicht gesagt, aber es gehört eine ganze Menge dazu, das so umzusetzen, dass es unseren hohen Ansprüchen genügt. Das fängt damit an, dass wir die passenden Anbauregionen und Sorten aufs genauste auswählen und nur mit Partner zusammenarbeiten, die unsere Werte und Leidenschaft für köstliche Früchte teilen.", "sanlucar")?></p>
+<p><?=_e("Damit das alles nicht nur schöne Worte sind, basiert unsere Unternehmensstrategie auf verschiedenen globalen Anforderungen und internationale Standards.", "sanlucar")?></p>
+
+<h3 class="mt-4"><?=_e("Ziele für nachhaltige Entwicklung","sanlucar")?></h3>
+<p><?=_e("Wir sind aktives Mitglied des spanischen Netzwerks der Vereinten Nationen und seit 2018 unterstützen wir die Ziele zur Nachhaltigkeitsentwicklung der Vereinten Nationen. Außerdem ist SanLucar 2011 dem Globalen Pakt der Vereinten Nationen (UN Global Compact) beigetreten und hat sich damit verpflichtet, alle Aktivitäten an zehn allgemein anerkannten Prinzipien in den Bereichen Menschenrechte, Arbeitsnormen, Umwelt und Korruptionsbekämpfung auszurichten. Die GRI-Indikatoren (Global Reporting Initiative) helfen uns dabei, die Einhaltung der Ziele und unseren Fortschritt in nachhaltiger Entwicklung zu messen.  Mehr Informationen zu den Aktivitäten und den 7 SDGs (3, 4, 6, 8, 10, 15 & 17), denen sich SanLucar verpflichtet, erhält man durch das Klicken auf folgendes Schema.","sanlucar")?></p>
+
+
+<div class="row">
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/1.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/2.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-3">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/3.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-4">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/4.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/5.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-6">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/6.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/7.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-8">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/8.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/9.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-10">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/10.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/11.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/12.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/13.png';  ?>" alt="">
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/14.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-15">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/15.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/16.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a class="coloricon" data-toggle="modal" data-target="#modal-17">
+        <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/17.png';  ?>" alt="">
+        </a>
+    </div>
+    <div class="col-6 col-md-2 mb-3">
+        <a href="" class="coloricon">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/coloricons/18.png';  ?>" alt="">
+        </a>
+    </div>
+</div>
+
+
+
+<h3 class="mt-4"><?=_e("Das SanLucar Auditierungssystem","sanlucar")?></h3>
+<p><?=_e("Wir haben hohe Qualitätsansprüche; nicht nur bei Obst und Gemüse, sondern auch bei Sozial- und Umweltaspekten. So haben wir ein eigenes internes Auditierungssystem in den Bereichen Governance, Handel, Produktion und Arbeitsmarktpolitik entwickelt. Es basiert auf den Grundlagen international anerkannter Zertifizierungen und Richtlinien aus dem Umwelt- und Sozialbereich. Unsere eigenen Kriterien sind jedoch strenger. Das Auditierungssystem hilft uns, unsere Aktivitäten zur Unternehmensverantwortung objektiv zu bewerten, vergleichbare Bedingungen auf den Farmen von SanLucar zu schaffen und konkrete Aktionspläne zu erstellen.","sanlucar")?></p>
+
+<h3 class="mt-4"><?=_e("Unser Anbauerprofil","sanlucar")?></h3>
+<p><?=_e("Bevor wir mit der Zusammenarbeit mit einem Anbaupartner beginnen, erstellen wir ein Anbauerprofil. Dies geschieht durch SanLucar - zugehörige Agraringenieure in Zusammenarbeit mit den Agraringenieuren der Anbaupartner. Nur wenn die Mindestanforderungen (wie GlobalGAP und/oder QS-Gap) und die Bewertungen für die Bereiche Soziales, Gesundheit, Sicherheit und Umwelt zufriedenstellend erfüllt sind, wird mit einer Zusammenarbeit begonnen. ","sanlucar")?></p>
+
+<h3 class="mt-4"><?=_e("Nachhaltig erfolgreich","sanlucar")?></h3>
+<p><?=_e("Die Balance zwischen Umwelt, Gesellschaft und Wirtschaft sind für uns zentrale Anliegen. Dabei setzen wir uns Ziele, die im Einklang stehen mit den Menschen und den Ressourcen in den Anbaugebieten.  Wir arbeiten mit unabhängigen Zertifizierungsstellen zusammen und erfüllen die Anforderungen international anerkannter Zertifizierungsstandards wie SA8000, Global GAP, GRASP und/oder Rainforest Alliance.","sanlucar")?></p>
+<p><?=_e(" Außerdem bündelt unser d.r.e.a.m.s. Programm unser soziales Engagement in den SanLucar Ländern. So haben wir 2018 für unsere kontinuierlichen Anstrengungen, die Arbeits- und Lebensbedingungen unserer Mitarbeiter zu verbessern, vom tunesischen Staat den Preis für sozialen Fortschritt erhalten. Der Preis wurde in einer feierlichen Zeremonie vom tunesischen Präsidenten an uns übergeben. Solche Auszeichnungen bestätigen unser nachhaltiges Tun und Handeln. Und wir sind stolz darauf.","sanlucar")?></p>
+
+<div class="row">
+    <div class="col-12 col-md-3">
+        <a href="">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/logos/logo1.jpg';  ?>" alt="" class="img-fluid">
+        </a>
+    </div>
+    <div class="col-12 col-md-3">
+        <a href="">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/logos/logo2.png';  ?>" alt="" class="img-fluid">
+        </a>
+    </div>
+    <div class="col-12 col-md-3">
+        <a href="">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/logos/logo3.png';  ?>" alt="" class="img-fluid">
+        </a>
+    </div>
+    <div class="col-12 col-md-3">
+        <a href="">
+            <img src="<?=  get_template_directory_uri()  .  '/../dist/images/logos/logo4.png';  ?>" alt="" class="img-fluid">
+        </a>
+
+    </div>
+</div>
+
+
+<div class="modal fade" id="modal-3" tabindex="-1" role="dialog" aria-labelledby="modal-3" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Gesundheit und Wohlergehen: Mit Sport, gesunder Ernährung und Arbeitsschutz tragen wir zu einem gesunden Körper und Geist bei.", "sanlucar")?></h3>
+                <p><?=_e("Das Vorbeugen mit Präventivmaßnahmen rund um den Arbeitsschutz und die Sicherheit, steht bei uns an allen Standorten an vorderster Stelle. Dafür sorgen unsere Experten und Betriebsärzte jeweils vor Ort. Regelmäßige Schulungen und Kampagnen zur Sensibilisierung unserer Mitarbeiter für Gesundheit und Sicherheit am Arbeitsplatz gehören dazu. Des Weiteren fördern Projekte in den Bereichen Sport und Ernährung einen gesunden Lebensstil innerhalb und außerhalb der Gemeinschaften. Um mehr über unsere einzelnen Projekte zu diesem Ziel zu erfahren, einfach hier weiterlesen. (Verlinken mit d.r.e.a.m.s. Projekten)", "sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-4" tabindex="-1" role="dialog" aria-labelledby="modal-4" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Hochwertige Bildung: Eine Kultur des Lernens.", "sanlucar")?></h3>
+                <p><?=_e("Da man Außergewöhnliches nur schaffen kann, wenn Menschen unterschiedlicher Kulturen und Denkungsarten friedlich miteinander arbeiten und sich gegenseitig mit ihren verschiedenen Sichtweisen bereichern, bieten wir allen den Raum den sie brauchen, um sich bestmöglich zu entfalten.", "sanlucar")?></p>
+                <p><?=_e("Schon bei den Jüngsten fangen wir damit ganz gezielt an. So stiften wir Kinder in den Communities unserer Fincas in Dritte-Welt Ländern Schulmaterialien und vergeben StipendienAuch alle Mitarbeiter weltweit bekommen eine Förderung ganz nach ihrem Geschmack, z.B. mit Sprachunterricht und Fortbildungsmaßnahmen, die genau auf sie zugeschnitten sind. Und in Spanien und Deutschland bieten wir das duale Ausbildungssystem an, mit dem man gleichzeitig bei uns im Büro arbeiten und an der Uni lernen kann - ein idealer Einstieg in den Traumberuf.", "sanlucar")?></p>
+                <p><?=_e("Denn wie hat schon John F. Kennedy gesagt: „nur eins ist teurer als Bildung: keine Bildung“.", "sanlucar")?></p>
+                <p><?=_e("Um mehr über unsere einzelnen Projekte zu diesem Ziel zu erfahren, einfach hier weiterlesen. (Verlinken mit d.r.e.a.m.s. Projekten)“.", "sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="modal-6" role="dialog" tabindex="-1" aria-labelledby="modal-6" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("    Sauberes Wasser: Wasser ist lebensnotwendig. Wir sorgen für eine effiziente Wassernutzung und sauberes Trinkwasser.","sanlucar")?></h3>
+                <p><?=_e("Wasser ist Leben. Deshalb gehen wir ganz sorgsam damit um und wenden modernste Anbaumethoden an, mit denen wir Wasser sparen können. Aber nicht nur die Menge ist wichtig, sondern auch die Qualität. Deshalb kontrollieren wir ständig, dass unsere Mitarbeiter weltweit Zugang zu sauberem Trinkwasser haben.","sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-8" role="dialog" tabindex="-1" aria-labelledby="modal-8" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Menschenwürdige Arbeit: Unsere Werte und der SanLucar-Ethikkodex stehen für Menschenrechte und faire Arbeitsbedingungen ","sanlucar")?></h3>
+                <p><?=_e("Höchste Priorität sind, neben einer fairen und pünktlichen Bezahlung, dauerhaft gerechte Arbeitsbedingungen und die Einhaltung der gesetzlichen Vorgaben. Mehrere Zertifizierungen helfen uns dabei, die Wertschöpfungskette auf Menschenrechte und faire Arbeitsbedingungen zu kontrollieren. Darüber hinaus, gehört die Unterstützung lokaler Lieferanten und regionale Mitarbeiterakquise zu unserer Unternehmenspolitik, ebenso wie die Förderung der Beschäftigung von jungen Menschen und deren Wachstumsmöglichkeiten. Um dies zu erreichen, gehen wir Kooperationen mit Universitäten an unseren Standorten ein und bieten außerdem ein duales Ausbildungssystem an (link Karriere). Um mehr über unsere einzelnen Projekte zu diesem Ziel zu erfahren, einfach hier weiterlesen. (Verlinken mit d.r.e.a.m.s. Projekten)","sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="modal fade" id="modal-10" role="dialog" tabindex="-1" aria-labelledby="modal-10" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Weniger Ungleichheit:  Für uns sind alle Menschen gleich. Dies zeigt sich in unseren Projekten für Menschen mit Behinderung, Schwache und Ausgegrenzte, Kinder und Erwachsene.","sanlucar")?></h3>
+                <p><?=_e("Diversität (Vielfalt) sehen wir als Chance und nicht als Hindernis. Sie gibt uns die Möglichkeit, die Vielfältigkeit von Kulturen, Sprachen und das unterschiedliche Wissen und die daraus resultierenden Kompetenzen zu nutzen. Um diesem Motto treu und immer auf dem richtigen Kurs zu bleiben, bestätigen externe Auditoren die Transparenz und die Erfüllung von fairen Arbeitsbedingungen und Löhnen in unserem Unternehmen. Um mehr über unsere einzelnen Projekte zu diesem Ziel zu erfahren, einfach hier weiterlesen. (Verlinken mit d.r.e.a.m.s. Projekten)","sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="modal fade" id="modal-15" role="dialog" tabindex="-1" aria-labelledby="modal-15" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Leben am Land","sanlucar")?></h3>
+                <p><?=_e("Da wir eng mit der Natur arbeiten, wissen wir, dass man immer das zurückbekommt, was man sät. Ökosysteme langfristig zu erhalten ist für uns dabei oberste Priorität. Sei es durch Recycling unseres Abfalls, Wiederaufforstung auf unseren Farmen oder Einsparung von CO2-Ausstoss durch Sonnenenergie-Nutzung. Und wo immer es die Qualitätserhaltung unserer Produkte zulässt, verzichten wir auf Plastik. Das sehen wir als unseren Beitrag, um unseren Nachkommen eine schöne Erde zu hinterlassen. Um mehr über unsere einzelnen Projekte zu diesem Ziel zu erfahren, einfach hier weiterlesen. (Verlinken mit d.r.e.a.m.s. Projekten)","sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="modal fade" id="modal-17" role="dialog" tabindex="-1" aria-labelledby="modal-17" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3><?=_e("Partnerschaften zur Erreichung der Ziele: Hinter jeder starken Firma stecken viele, noch stärkere Partnerschaften","sanlucar")?></h3>
+                <p><?=_e("Um die Wurzeln der Menschen auf unseren Farmen haben zu stärken, arbeiten wir mit Ministerien, Universitäten, NGOs (Nicht-Regierungs-Organisationen), öffentlichen Einrichtungen und Vereinen zusammen. So sind viele gute Partnerschaften auf der ganzen Welt entstanden, die reichlich Früchte tragen. Unsere Projekte konzentrieren sich auf Bildung, Sport und Integration von benachteiligten Gruppen wie beispielsweise Menschen mit Behinderungen und geringem Einkommen. Seit 2018 haben wir mit SDG (Sustainable Development Goals) Ziele, an denen wir uns gern auch messen lassen.","sanlucar")?></p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
