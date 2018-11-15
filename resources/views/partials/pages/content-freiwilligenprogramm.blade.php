f471d67c8718a4ab7e847c4a589c53ac45b9c264
Gus <gustavojuan@gmail.com>
2018-11-15 12:37:17 +0100

    team

--- resources/views/partials/pages/content-freiwilligenprogramm.blade.php
+++ resources/views/partials/pages/content-freiwilligenprogramm.blade.php
@@ -1,13 +1,51 @@
-<h2 class="text-center big"><?=_e("Bei uns scheint für alle die Sonne","sanlucar")?></h2>
+<h2 class="text-center big"><?=_e( "Bei uns scheint für alle die Sonne", "sanlucar" )?></h2>
 
-<p><?=_e("Wie ist das eigentlich vor Ort mit anzupacken? Unsere Mitarbeiter können für einen Monat auf den Farmen in Ecuador, Südafrika und Tunesien selbst erleben, was d.r.e.a.m.s. alles kann.","sanlucar")?></p>
-<p><?=_e("Hier ein paar Stimmen von denen, die mitgemacht haben. ","sanlucar")?></p>
+<p><?=_e( "Wie ist das eigentlich vor Ort mit anzupacken? Unsere Mitarbeiter können für einen Monat auf den Farmen in Ecuador, Südafrika und Tunesien selbst erleben, was d.r.e.a.m.s. alles kann.", "sanlucar" )?></p>
+<p><?=_e( "Hier ein paar Stimmen von denen, die mitgemacht haben. ", "sanlucar" )?></p>
 
 
+<div class="row">
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/malaika.png';  ?>" alt="">
+        <h3>Malaika Bertó </h3>
+        <em class="mb-3"><?=_e( "Back Office Coordinator", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„Ich war in Südafrika und erinnere mich vor allem an die herzlichen und fröhlichen Menschen, die ich getroffen habe. Einen bleibenden Eindruck haben für mich die kulturellen Unterschiede und die Armut hinterlassen. Dort habe ich gelernt, dass man zum Glücklichsein nicht viel benötigt.“", "sanlucar" )?></p>
+    </div>
 
-<div class="col-12 col-md-6 text-center">
-    <img src="http://placehold.it/135x135" alt="" class="rounded-circle mb-4">
-    <h3>Malaika Bertó </h3>
-    <em><?=_e("Back Office Coordinator", "sanlucar")?></em>
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/nathalie.png';  ?>" alt="">
+        <h3>Nathalie Schäfer</h3>
+        <em class="mb-3"><?=_e( "Sales Manager", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„In Südafrika habe ich sehr viel Dankbarkeit erfahren. Denn für die Bewohner hat sich durch SanLucar vieles zum Besseren verändert. Die Kinder sind kleine Sonnenscheine, die stets lachen und singen. Über den Anbau von Zitrusfrüchten, Feigen und Trauben habe ich auch einiges gelernt.“", "sanlucar" )?></p>
+    </div>
+
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/meiyi.png';  ?>" alt="">
+        <h3>Meiyi Hou </h3>
+        <em class="mb-3"><?=_e( "Commercial Strategy Analist", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„Ich bin nach Rooihoogte gegangen, um zu lernen und um gemeinsam die Welt ein klein bisschen besser zu machen. Mir gefällt die Arbeit mit Kindern. Und natürlich ist es schwer, innerhalb eines Monats Mathe-, Informatik- und Musikkenntnisse zu verbessern. Doch es ist möglich, den Kindern einen Gedanken zu vermitteln: Die Welt ist groß und schön, und wir brauchen Neugierde, um sie zu entdecken.“", "sanlucar" )?></p>
+    </div>
+
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/iris.png';  ?>" alt="">
+        <h3>Iris Rechinger</h3>
+        <em class="mb-3"><?=_e( "Recruitment and <br>Selection Technician", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„Das Volontariat in Südafrika war für mich eine einzigartige Erfahrung. Neben der Möglichkeit, den Lebensalltag der Südafrikaner zu entdecken, konnte ich auch sehr viel über mich selbst lernen. In einem Unternehmen wie SanLucar zu arbeiten, wo man solche Möglichkeiten bekommt und sieht wie sehr sich alle gesellschaftlich engagieren, ist ein tolles Gefühl.“", "sanlucar" )?></p>
+    </div>
+
+
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/zuzana.png';  ?>" alt="">
+        <h3>Zuzana Holesova</h3>
+        <em class="mb-3"><?=_e( "Sales Manager", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„Ich war auf der Hacienda Magdalena in Ecuador – und ich war begeistert. Da ich Kontakt mit Kunden und unseren Partnern habe, ist es für mich jetzt leichter, über unsere Anbaumethoden zu sprechen. Jetzt verstehe ich auch das geschäftliche Risiko in Ländern wie Ecuador besser, und wie positiv sich die Arbeit von SanLucar auswirkt.“", "sanlucar" )?></p>
+    </div>
+
+    <div class="col-12 col-md-6 text-center mb-5">
+        <img class="mb-4" src="<?=  get_template_directory_uri()  .  '/../dist/images/people/germain.png';  ?>" alt="">
+        <h3>Germain Gillerot</h3>
+        <em class="mb-3"><?=_e( "Commercial Planning <br>Consultant", "sanlucar" )?></em>
+        <p class="text-justify p-4"><?=_e( "„Die Teilnahme am SanLucar Freiwilligenprogramm war eine Gelegenheit, die ich nicht missen möchte! Ich hatte eine wunderbare Zeit und bekam die Chance, ein Permakultur-Gartenprojekt zu starten, das der Kinderkrippe gesunde Früchte und Gemüse liefert. Ich war erstaunt, wie sich die all die Menschen dort in soziale Projekte einbringen!”", "sanlucar" )?></p>
+    </div>
 </div>
 
