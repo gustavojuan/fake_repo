6461b01462b79f9708eee3733d7c8d147ed4eb60
Roberto <roberto@creativados.com>
2018-11-15 16:18:57 +0100

    Merge branch 'master' of http://git.ahoraencreativados.com/Creativados/SanLucarTheme
    # Conflicts:
    #	.idea/SanLucarTheme.iml
    #	.idea/deployment.xml
    #	.idea/workspace.xml

--- resources/views/partials/pages/content-stellenangebote.blade.php
+++ resources/views/partials/pages/content-stellenangebote.blade.php
@@ -1,5 +1,8 @@
 <div class="row">
   <div class="col-12">
+    <h2 class="text-center big"><?= _e( "Stellenangebote", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
     <h2><?= _e( "Stelle gesucht, Karriere gefunden", "sanlucar" ); ?></h2>
   </div>
   <div class="col-12">
