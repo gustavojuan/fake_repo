820fe8b667ab18057c57ec9c396cf01aae4a744f
Roberto <roberto@creativados.com>
2018-11-15 11:42:15 +0100

    added deine karreire bei Sanlucar page

---
+++ resources/views/partials/pages/content-deine-karriere-bei-sanlucar.blade.php
@@ -0,0 +1,37 @@
+<h2><?=_e( "Ob erfahrene Fachkraft, Berufsanfänger oder Praktikant: SanLucar bietet attraktive Karrierewege für jeden Abschnitt des Berufslebens. Wir freuen uns auf deine Bewerbung!", "sanlucar" )?></h2>
+
+<section class="button-wrapper my-5 py-5">
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="#" class="page_button">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/icons/arbeiten.png';  ?>" alt="">
+        <h2><?php _e( "Arbeiten bei SanLucar", "sanlucar" )?></h2>
+      </a>
+    </div>
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="#" class="page_button">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/icons/cv.png';  ?>" alt="">
+        <h2><?php _e( "Stellenangebote", "sanlucar" )?></h2>
+      </a>
+    </div>
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="#" class="page_button">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/icons/dualfp.png';  ?>" alt="">
+        <h2><?php _e( "Duale Ausbildung", "sanlucar" )?></h2>
+      </a>
+    </div>
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="#" class="page_button">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/icons/student.png';  ?>" alt="">
+        <h2><?php _e( "Unser Praktikumsprogramm ", "sanlucar" )?></h2>
+      </a>
+    </div>
+    <div class="col-12 mt-4">
+      <a href="#" class="page_button">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/icons/cercled-people.png';  ?>" alt="">
+        <h2><?php _e( "Funktionsbereiche", "sanlucar" )?></h2>git
+      </a>
+    </div>
+
+  </div>
+</section>
