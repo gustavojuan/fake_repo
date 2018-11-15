bcd82e60f2e59c04179ccbf29c6476b40f5d3507
Roberto <roberto@creativados.com>
2018-11-15 13:55:59 +0100

    Merge branch 'master' of http://git.ahoraencreativados.com/Creativados/SanLucarTheme
    # Conflicts:
    #	.idea/SanLucarTheme.iml
    #	.idea/deployment.xml
    #	.idea/workspace.xml

---
+++ resources/views/partials/pages/content-funktionsbereiche.blade.php
@@ -0,0 +1,67 @@
+<div class="row">
+  <div class="col-12">
+    <h2 class="text-center big"><?= _e( "Funktionsbereiche", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <h2><?= _e( "Finde deinen Traumjob", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <p><?= _e( "Genauso vielfältig wie unsere Produkte, sind bei uns die beruflichen Möglichkeiten:" ); ?></p>
+  </div>
+  {{--Buttons--}}
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Branding", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Commercial Planning", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Communication & CR", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Distribution", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Finance, Strategy & IT", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Human Resources & Office Management", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Legal", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Operations: Packaging, Quality, Logistics, Warehouse", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Production and R&D", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2 mb-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Sales & Product Management", "sanlucar" )?></h2>
+    </a>
+  </div>
+  <div class="col-8 offset-2">
+    <a href="#" class="page_button rounded_border">
+      <h2><?= _e( "Travel Management & Executive Assistance", "sanlucar" )?></h2>
+    </a>
+  </div>
+</div>
