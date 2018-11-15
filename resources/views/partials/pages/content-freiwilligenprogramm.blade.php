e45d197b1f69b0eeb3531da881e4d1c33606169c
Gus <gustavojuan@gmail.com>
2018-11-15 11:47:18 +0100

    gitginotre

---
+++ resources/views/partials/pages/content-freiwilligenprogramm.blade.php
@@ -0,0 +1,13 @@
+<h2 class="text-center big"><?=_e("Bei uns scheint für alle die Sonne","sanlucar")?></h2>
+
+<p><?=_e("Wie ist das eigentlich vor Ort mit anzupacken? Unsere Mitarbeiter können für einen Monat auf den Farmen in Ecuador, Südafrika und Tunesien selbst erleben, was d.r.e.a.m.s. alles kann.","sanlucar")?></p>
+<p><?=_e("Hier ein paar Stimmen von denen, die mitgemacht haben. ","sanlucar")?></p>
+
+
+
+<div class="col-12 col-md-6 text-center">
+    <img src="http://placehold.it/135x135" alt="" class="rounded-circle mb-4">
+    <h3>Malaika Bertó </h3>
+    <em><?=_e("Back Office Coordinator", "sanlucar")?></em>
+</div>
+
