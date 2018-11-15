c0436a920cec5a64720526136f1a4ddb2069d635
Roberto <roberto@creativados.com>
2018-11-15 13:03:19 +0100

    added arbeiten bei sanlucar page

---
+++ resources/views/partials/pages/content-arbeiten-bei-sanlucar.blade.php
@@ -0,0 +1,31 @@
+<div class="row">
+  <div class="col-12">
+    <h2 class="text-center big"><?php _e( "Arbeiten bei SanLucar", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <h2><?php _e( "Werde SanLucaner!", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <p><?php _e( "Wir tragen keine Anzüge, duzen uns, sind eine multikulturelle bunte Truppe aus aller Welt, arbeiten gern im Team,
+      mögen flache Hierarchien und lieben unser Obst und Gemüse.
+      Wem das genauso gefällt, wie unsere Mission, Vision und Werte, dem ermöglichen wir Karrierewege in allen
+      Abschnitten des Berufslebens - egal ob als erfahrene Fachkraft, Einsteiger oder Praktikant.
+      Bewerbungen sind jederzeit willkommen. Und damit sich Bewerber schon während des Auswahlverfahrens gut aufgehoben
+      fühlen, ist immer ein fester Ansprechpartner aus der Personalabteilung für einen da. Während des Assessments lernt
+      man seinen Manager kennen und natürlich möchten auch wir dich kennenlernen und gemeinsam mit dir herausfinden, wo
+      Deine Talente liegen und was dir besonders Spaß macht. Wir freuen uns darauf!", "sanlucar" ); ?>
+    </p>
+  </div>
+  <div class="col-12 text-center">
+    <img src="<?=  get_template_directory_uri() . '/../dist/images/people/workers-5.jpg';?>" alt="">
+  </div>
+</div>
+<section class="button-wrapper mt-3 text-center">
+  <div class="row">
+    <div class="col-12">
+      <a href="#" class="page_button">
+        <h2><?php _e( "Werde SanLucaner!", "sanlucar" )?></h2>
+      </a>
+    </div>
+  </div>
+</section>
