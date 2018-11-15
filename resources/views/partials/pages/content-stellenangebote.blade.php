034cd968effbb3f31f1c25b570a6d4db11ef5d34
Roberto <roberto@creativados.com>
2018-11-15 16:18:38 +0100

    added stellenangebote page

---
+++ resources/views/partials/pages/content-stellenangebote.blade.php
@@ -0,0 +1,21 @@
+<div class="row">
+  <div class="col-12">
+    <h2><?= _e( "Stelle gesucht, Karriere gefunden", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <p><?= _e( "Wenn du unsere Mission, Vision, Werte mit uns teilst, und Lust hast, tagtäglich mit deinem Team dein Bestes zu geben, dann gleich bewerben.", "sanlucar" ); ?></p>
+  </div>
+  <div class="col-12 text-center">
+    <img src="<?=  get_template_directory_uri() . '/../dist/images/people/workers-6.jpg';?>" alt="">
+  </div>
+  <div class="col-12"></div>
+</div>
+<section class="button-wrapper mt-3 text-center">
+  <div class="row">
+    <div class="col-6 offset-3">
+      <a href="#" class="page_button">
+        <h2 class="button_big"><?= _e( "Aktuelle Stellenangebote", "sanlucar" )?></h2>
+      </a>
+    </div>
+  </div>
+</section>
