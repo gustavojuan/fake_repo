d320f63601c0106b4a11623cfcb7df834af4ae3d
Roberto <roberto@creativados.com>
2018-11-15 18:20:32 +0100

    addded ausbildung page

--- resources/views/partials/pages/content-duale-ausbildung.blade.php
+++ resources/views/partials/pages/content-duale-ausbildung.blade.php
@@ -11,7 +11,7 @@
     </p>
   </div>
   <div class="col-12">
-    <h2 class="list_title"><?= _e( "Dein Profil:", "sanlucar" ); ?></h2>
+    <h3><?= _e( "Dein Profil:", "sanlucar" ); ?></h3>
     <ul>
       <li><p class="mb-0">Mindestens mittlere Reife für die Ausbildung in Deutschland/ Abitur oder Fachabitur für die
           Ausbildung in Spanien</p></li>
@@ -25,7 +25,7 @@
     </ul>
   </div>
   <div class="col-12">
-    <h2 class="list_title"><?= _e( "Wir bieten:", "sanlucar" ); ?></h2>
+    <h3><?= _e( "Wir bieten:", "sanlucar" ); ?></h3>
     <ul>
       <li><p class="mb-0">Vielfältige Aufgaben und Verantwortung ab dem ersten Tag</p></li>
       <li><p class="mb-0">Obst- und Gemüselehrgänge, Sprachkurse und Förderung </p></li>
@@ -49,14 +49,87 @@
       Jeden Tag übernimmst du mehr und mehr Verantwortung und schließt deine Ausbildung nach drei Jahren mit einer staatlich anerkannten Prüfung ab.", "sanlucar" );?>
     </p>
   </div>
+</div>
+<div class="row">
   <div class="col-12">
-    <h2 class="list_title">
+    <h2>
       <?= _e( "Deine Ausbildung im Überblick: ", "sanlucar" ); ?>
     </h2>
   </div>
 </div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Ausbildungsbeginn", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "Jeweils am 1.9.", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Einsatzort", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table">
+    <h2><?= _e( "Ettlingen (Karlsruhe) Einsätze im Ausland möglich", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Ausbildungszeit", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "3 Jahre", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Perspektive", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "Gute Übernahmechancen", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row mt-3">
+  <div class="col-12 text-center">
+    <a href="#" class="page_button">
+      <h2 class="button_big text-center"><?= _e( "Bewirb dich jetzt!", "sanlucar" )?></h2>
+    </a>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <h2><?= _e( "Duale Ausbildung in Spanien", "sanlucar" ); ?></h2>
+  </div>
+  <div class="col-12">
+    <p>
+      <?= _e( "Als erstes Unternehmen in Spanien bieten wir die Ausbildung zum Kaufmann im Groß- und Außenhandel an. Wir orientieren uns dabei am System der dualen Ausbildung, wie wir sie seit vielen Jahren mit großem Erfolg in Deutschland umsetzen – mit einer Kombination aus Theorie und Praxis: Die theoretischen Grundlagen für den Beruf des Kaufmanns im Groß- und Außenhandel vermitteln wir in Valencia in der SanLucar Academy in Zusammenarbeit mit der FEDA Business School in Madrid. Neben den hochqualifizierten Lehrern der FEDA profitierst du von unseren Praxisexperten, die regelmäßig Vorträge in der SanLucar Academy halten. Im Laufe deiner Ausbildung wirst du zu einem wahren Obst- und Gemüseexperten. Das Gelernte wendest du dann direkt in der Praxis bei uns in Valencia an.
+Nach zwei Jahren schließt du die Ausbildung mit einer von der Deutschen Handelskammer anerkannten Prüfung zum Kaufmann im Groß- und Außenhandel (m/w) ab.
+Du kannst deine Bewerbung zentral an die FEDA oder direkt an SanLucar senden.", "sanlucar" );?>
+    </p>
+  </div>
+</div>
 <div class="row">
-  <div class="col-3 box_table"><h2 class="box_table">Ausbildungsbeginn</h2></div>
-  <div class="col-5 box_table"><h2 class="box_table">Jeweils am 1.9.</h2></div>
+  <div class="col-12">
+    <h2>
+      <?= _e( "Deine Ausbildung im Überblick: ", "sanlucar" ); ?>
+    </h2>
+  </div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Ausbildungsbeginn", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "Jeweils am 1.9.", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Einsatzort", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table">
+    <h2><?= _e( "Puzol (Valencia), Einsätze im Ausland möglich", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Ausbildungszeit", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "2 Jahre", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row ml-1">
+  <div class="col-12 col-md-3 box_table"><h2><?= _e( "Perspektive", "sanlucar" ); ?></h2></div>
+  <div class="col-12 col-md-7 box_table"><h2><?= _e( "Gute Übernahmechancen", "sanlucar" ); ?></h2></div>
+</div>
+<div class="row mt-3">
+  <div class="col-12 text-center">
+    <a href="#" class="page_button">
+      <h2 class="button_big text-center"><?= _e( "Bewirb dich jetzt!", "sanlucar" )?></h2>
+    </a>
+  </div>
+</div>
+
+<div class="row text-center">
+  <h2><?= _e( "Hier erzählt Azubi Heike über ihre Ausbildung bei uns.", "sanlucar" )?></h2>
+</div>
+
+<div class="row text-center">
+  <img style="border: 3px solid blue; border-radius: 100%" src="<?=  get_template_directory_uri() . '/../dist/images/img-lady-circle.png';?>" alt="">
 </div>
 
