01ae090198e258f85e8d008a75e4c0b45b09a63f
Gus <gustavojuan@gmail.com>
2018-11-15 08:57:20 +0100

    Cambios DERAMS

--- resources/views/partials/pages/content-bildung.blade.php
+++ resources/views/partials/pages/content-bildung.blade.php
@@ -51,9 +51,13 @@
 <div class="row" id="dreams_buttons">
     <div class="col-12 text-center">
         <ul class="list-unstyled">
+            <!-- TODO -->
             <li class="d-inline-block"><a href="."><img  class="dream_button active" src="<?=  get_template_directory_uri() . '/../dist/images/icons/bildung.png';  ?>" alt=""></a></li>
-            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/bildung.png';  ?>" alt=""></a></li>
-            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/bildung.png';  ?>" alt=""></a></li>
+            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/ernahrung.png';  ?>" alt=""></a></li>
+            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/sport.png';  ?>" alt=""></a></li>
+            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/erde.png';  ?>" alt=""></a></li>
+            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/wasser.png';  ?>" alt=""></a></li>
+            <li class="d-inline-block"><a href="."><img  class="dream_button" src="<?=  get_template_directory_uri() . '/../dist/images/icons/luft.png';  ?>" alt=""></a></li>
         </ul>
     </div>
 </div>
