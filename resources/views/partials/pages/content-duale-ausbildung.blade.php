525aee7be7096426fd877bd3d542942b24e260dd
Roberto <roberto@creativados.com>
2018-11-16 11:59:23 +0100

    Merge branch 'master' of http://git.ahoraencreativados.com/Creativados/SanLucarTheme
    # Conflicts:
    #	.idea/SanLucarTheme.iml
    #	.idea/deployment.xml
    #	.idea/workspace.xml

--- resources/views/partials/pages/content-duale-ausbildung.blade.php
+++ resources/views/partials/pages/content-duale-ausbildung.blade.php
@@ -13,25 +13,33 @@
   <div class="col-12">
     <h3><?= _e( "Dein Profil:", "sanlucar" ); ?></h3>
     <ul>
-      <li><p class="mb-0">Mindestens mittlere Reife für die Ausbildung in Deutschland/ Abitur oder Fachabitur für die
-          Ausbildung in Spanien</p></li>
-      <li><p class="mb-0">Du verfügst über gute Deutsch- und Englischkenntnisse, für die Ausbildung in Spanien natürlich
-          auch gute --Spanischkenntnisse</p></li>
-      <li><p class="mb-0">Du bist sehr kommunikativ und willst in einem dynamischen Umfeld arbeiten</p></li>
-      <li><p class="mb-0">Zu deinen Stärken zählt eine rasche Auffassungsgabe, Motivation und Proaktivität</p></li>
-      <li><p class="mb-0">Deine Hauptmotivation ist es, in einem internationalen Umfeld zu lernen und dich zu
-          entwickeln, indem du die Praxis --täglich lebst – learning by doing!</p></li>
-      <li><p class="mb-0">Du liebst Obst und Geyarn startmüse</p></li>
+      <li><p class="mb-0"><?= _e( "Mindestens mittlere Reife für die Ausbildung in Deutschland/ Abitur oder Fachabitur für die
+          Ausbildung in Spanien", "sanlucar" ); ?></p></li>
+      <li><p class="mb-0"><?= _e( "Du verfügst über gute Deutsch- und Englischkenntnisse, für die Ausbildung in Spanien natürlich
+          auch gute --Spanischkenntnisse", "sanlucar" ); ?></p></li>
+      <li><p
+          class="mb-0"><?= _e( "Du bist sehr kommunikativ und willst in einem dynamischen Umfeld arbeiten", "sanlucar" ); ?></p>
+      </li>
+      <li><p
+          class="mb-0"><?= _e( "Zu deinen Stärken zählt eine rasche Auffassungsgabe, Motivation und Proaktivität", "sanlucar" ); ?></p>
+      </li>
+      <li><p class="mb-0"><?= _e( "Deine Hauptmotivation ist es, in einem internationalen Umfeld zu lernen und dich zu
+          entwickeln, indem du die Praxis --täglich lebst – learning by doing!", "sanlucar" ); ?></p></li>
+      <li><p class="mb-0"><?= _e( "Du liebst Obst und Geyarn startmüse", "sanlucar" ); ?></p></li>
     </ul>
   </div>
   <div class="col-12">
     <h3><?= _e( "Wir bieten:", "sanlucar" ); ?></h3>
     <ul>
-      <li><p class="mb-0">Vielfältige Aufgaben und Verantwortung ab dem ersten Tag</p></li>
-      <li><p class="mb-0">Obst- und Gemüselehrgänge, Sprachkurse und Förderung </p></li>
-      <li><p class="mb-0">Eine individuelle Betreuung durch unsere Manager, Ausbilder und netten Kollegen</p></li>
-      <li><p class="mb-0">Gute Übernahmechancen mit tollen Karrieremöglichkeiten</p></li>
-      <li><p class="mb-0">Leckere Produkte – regelmäßig führen wir Verkostungen unseres Obsts und Gemüses durc</p></li>
+      <li><p class="mb-0"><?= _e( "Vielfältige Aufgaben und Verantwortung ab dem ersten Tag", "sanlucar" ); ?></p></li>
+      <li><p class="mb-0"><?= _e( "Obst- und Gemüselehrgänge, Sprachkurse und Förderung", "sanlucar" ); ?> </p></li>
+      <li><p
+          class="mb-0"><?= _e( "Eine individuelle Betreuung durch unsere Manager, Ausbilder und netten Kollegen", "sanlucar" ); ?></p>
+      </li>
+      <li><p class="mb-0"><?= _e( "Gute Übernahmechancen mit tollen Karrieremöglichkeiten", "sanlucar" ); ?></p></li>
+      <li><p
+          class="mb-0"><?= _e( "Leckere Produkte – regelmäßig führen wir Verkostungen unseres Obsts und Gemüses durc", "sanlucar" ); ?></p>
+      </li>
     </ul>
   </div>
 </div>
@@ -50,7 +58,7 @@
     </p>
   </div>
 </div>
-<div class="row">
+<div class="row mt-3">
   <div class="col-12">
     <h2>
       <?= _e( "Deine Ausbildung im Überblick: ", "sanlucar" ); ?>
@@ -75,9 +83,9 @@
   <div class="col-12 col-md-7 box_table"><h2><?= _e( "Gute Übernahmechancen", "sanlucar" ); ?></h2></div>
 </div>
 <div class="row mt-3">
-  <div class="col-12 text-center">
+  <div class="col-4 offset-4">
     <a href="#" class="page_button">
-      <h2 class="button_big text-center"><?= _e( "Bewirb dich jetzt!", "sanlucar" )?></h2>
+      <h2 class="button_big mb-1"><?= _e( "Bewirb dich jetzt!", "sanlucar" )?></h2>
     </a>
   </div>
 </div>
@@ -118,18 +126,85 @@ Du kannst deine Bewerbung zentral an die FEDA oder direkt an SanLucar senden.", 
   <div class="col-12 col-md-7 box_table"><h2><?= _e( "Gute Übernahmechancen", "sanlucar" ); ?></h2></div>
 </div>
 <div class="row mt-3">
-  <div class="col-12 text-center">
+  <div class="col-4 offset-4 text-center">
     <a href="#" class="page_button">
       <h2 class="button_big text-center"><?= _e( "Bewirb dich jetzt!", "sanlucar" )?></h2>
     </a>
   </div>
 </div>
 
-<div class="row text-center">
-  <h2><?= _e( "Hier erzählt Azubi Heike über ihre Ausbildung bei uns.", "sanlucar" )?></h2>
+<div class="row text-center mt-5">
+  <div class="col-12">
+    <h2><?= _e( "Hier erzählt Azubi Heike über ihre Ausbildung bei uns.", "sanlucar" )?></h2>
+  </div>
+  <div class="col-4 offset-4 mt-3">
+    <img class="circled_border"
+         src="<?=  get_template_directory_uri() . '/../dist/images/img-lady-circle.png';?>" alt="">
+  </div>
+  <div class="col-12 mt-3">
+    <h3><?= _e( "Heike Grol, 20", "sanlucar" )?></h3>
+  </div>
+  <div class="col-12">
+    <p><?= _e( "aus Norddeutschland absolviert seit 2017 bei SanLucar eine berufliche Ausbildung.", "sanlucar" )?></p>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <h2><?= _e( "Weshalb hast Du Dich für eine duale Ausbildung entschieden?", "sanlucar" )?></h2>
+    <p><?= _e( "Ich liebe es, in anderen Ländern zu leben und andere Sprachen zu sprechen. Mein Lehrer in Norddeutschland erzählte mir von der deutschen dualen Berufsausbildung FEDA (Formación Empresarial Dual Alemana) in Spanien. Dieses Programm bietet beiden Seiten Vorteile: den Azubis, weil ihnen nicht nur abstrakte theoretische Kenntnisse vermittelt wird, und den Unternehmen, weil sie ihre zukünftigen Mitarbeiter nach ihren eigenen Bedürfnissen ausbilden können. Das Ziel ist nämlich, dass die Azubis nach Abschluss ihrer Ausbildung weiter bei den Unternehmen arbeiten.", "sanlucar" )?></p>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <h2><?= _e( "Warum SanLucar?", "sanlucar" )?></h2>
+    <p><?= _e( "Mich haben mehrere Unternehmen zu einem Vorstellungsgespräch eingeladen, doch von Anfang an hat mir SanLucar am besten gefallen.
+    Ich bin sehr beeindruckt von der Geschichte dieses jungen, multinationalen Unternehmens und identifiziere mich seiner Unternehmensphilosophie.", "sanlucar" )?></p>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <h2><?= _e( "Wie ist die duale Ausbildung aufgebaut?", "sanlucar" )?></h2>
+    <p><?= _e( "Die Ausbildung dauert zwei  Jahre. 65 Prozent der Zeit verbringt man im Ausbildungsbetrieb und 35 Prozent in der Berufsschule. Bei SanLucar arbeite ich in verschiedenen Abteilungen, um unterschiedliche Tätigkeiten zu lernen.
+    Ich habe in der Verwaltung angefangen, doch meine Ausbildung ist vor allem auf die Vertriebsabteilungen ausgerichtet.", "sanlucar" )?></p>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <h2><?= _e( "Welche Erfahrungen hast Du bisher gemacht?", "sanlucar" )?></h2>
+    <p><?= _e( "Nur gute! Bei SanLucar fühle ich mich sehr wohl. Mir macht es viel Spaß, mich in Spanisch, Englisch und Deutsch zu verständigen,
+    das Arbeitsklima ist  sehr familiär.
+     Ich möchte noch viel mehr lernen und meine Erfahrungen im Vertrieb vertiefen. ", "sanlucar" )?></p>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col-12">
+    <p><a style="font-size: 12px"
+          href=""><?= _e( "Hier", "sanlucar" )?></a> <?= _e( "findest du mehr über Heikes Ausbildung in Spanien heraus!", "sanlucar" )?>
+    </p>
+  </div>
 </div>
 
-<div class="row text-center">
-  <img style="border: 3px solid blue; border-radius: 100%" src="<?=  get_template_directory_uri() . '/../dist/images/img-lady-circle.png';?>" alt="">
+<div class="row mt-5">
+  <div class="col-5">
+    <div class="row">
+      <div class="col-6">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/logos/ahk.jpg';?>" alt="">
+      </div>
+      <div class="col-6">
+        <h3>Deutsvda klmda mkmsssas</h3>
+        <p>jjksoroens solsaoe dpd</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-3 offset-3">
+    <div class="row">
+      <div class="col-6">
+        <img src="<?=  get_template_directory_uri() . '/../dist/images/logos/feda.jpg';?>" alt="">
+      </div>
+      <div class="col-6">
+        <h3>adasdadasdewas</h3>
+        <p>adsaewasdafewsdadxa</p>
+      </div>
+    </div>
+  </div>
 </div>
-s
