40ca7125db43af4b8c1f27e03b5750c37ab3a359
Roberto <roberto@creativados.com>
2018-11-19 16:19:05 +0100

    Merge branch 'master' of http://git.ahoraencreativados.com/Creativados/SanLucarTheme
    # Conflicts:
    #	.idea/SanLucarTheme.iml
    #	.idea/deployment.xml
    #	.idea/workspace.xml

--- resources/views/partials/pages/content-pressemeldungen.blade.php
+++ resources/views/partials/pages/content-pressemeldungen.blade.php
@@ -2,34 +2,85 @@
 $args = array(
   'orderby'        => 'date',
   'post_type'      => 'post',
-  'posts_per_page' => '-1'
+  'posts_per_page' => '6'
 );
 $the_query = new WP_Query( $args );
+$post_count = wp_count_posts()->publish;
 ?>
+<div class="border_bot noticias_destacado">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <?php for($i = 0; $i <= $post_count; $i ++){?>
+      <li data-target="#carouselExampleIndicators" data-slide-to=<?php $i ?> class="<?php if ( $i == 0 )
+        echo 'active'?>"></li>
+      <?php } ?>
+    </ol>
+    <div class="carousel-inner">
+    <?php if ( $the_query->have_posts() ) : ?>
 
-<div class="carousel-inner" style="height: 500px">
-  <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
-  ?>
-  <div class="carousel-item <?php if ( $the_query->current_post == 0 ) : ?>active<?php endif; ?>">
-    <?php if ( has_post_thumbnail() ) : ?>
-    <div class="row">
-      <div class="col-6"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-          <?php the_post_thumbnail( ); ?>
-        </a></div>
-      <div class="col-6">
-        <?php the_content();?>
-      </div>
-    </div>
-    <?php endif; ?>
+    <!-- pagination here -->
 
-    <div class="container">
-      <div class="carousel-caption text-left">
-        <h1><?php the_title(); ?></h1>
-        <p class="d-none d-sm-block"><?php the_excerpt(); ?></p>
+      <!-- the loop -->
+      <?php  $counter = 0; ?>
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+      <div class="carousel-item <?php if ( $counter == 0 ) {
+        echo 'active';
+      }?>">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img src="<?php  the_post_thumbnail_url() ?>" class="slide_img" alt="">
+          </div>
+          <div class="col-12 col-md-6">
+            <h2><?= the_title()?></h2>
+            <p><?php the_excerpt(7 )?></p>
+            <a href="<?php get_post_permalink() ?>"><?php _e( "Mehr lesen", "sanlucar" )?></a>
+          </div>
+        </div>
+        {{--  <img class="d-block w-100" src="http://placehold.it/1444x1000" alt="First slide">--}}
       </div>
+
+      <?php  $counter = $counter + 1; ?>
+      <?php endwhile; ?>
+    <!-- end of the loop -->
+
+      <!-- pagination here -->
+
+      <?php wp_reset_postdata(); ?>
+
+      <?php else : ?>
+      <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
+
     </div>
-  </div><!-- /.carousel-item -->
-  <!-- end second loop -->
-  <?php endwhile;  endif; ?>
-</div><!-- /.carousel-inner -->
-</div><!-- /.carousel-slide -->
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<h3><?php _e( "SanLucar News", "sanlucar" );?></h3>
+
+<div class="row">
+  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+  <div class="col-12 col-md-4">
+    <div>
+      <img src="<?php  the_post_thumbnail_url() ?>" class="slide_img" alt="">
+    </div>
+    <h2><?php get_the_title()?></h2>
+    <p class="border_bot"><?php echo get_the_excerpt(8)?></p>
+    <p><?php the_date() ?></p>
+  </div>
+
+  <?php endwhile; ?>
+<!-- end of the loop -->
+
+  <!-- pagination here -->
+
+  <?php wp_reset_postdata(); ?>
+</div>
+
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
+  <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );?>
+  <div class="row page_bg page-height " id="{{$post_slug}}"
+       style="background-image: url('<?php echo $backgroundImg[0]; ?>');">
+    <img src="" alt="">
+    <div class="container">
+      <div class="row h-100">
+        <div class="col-12 col-md-8 offset-md-2 blank_bg">
+          <div class="row">
+            <div class="col-12 text-center">
+              <h1><?=the_title()?></h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 px-5 py-2">
+              {{--@include($partial)--}}
+              <?php the_content()?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
   @endwhile
 @endsection
+
